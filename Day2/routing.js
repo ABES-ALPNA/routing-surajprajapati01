cdcc3c7da5a7aad6e3ad9f6271da6d054c994a10
surajprajapati01 <surajpj33@gmail.com>
2025-08-27 10:27:35 +0530

    day 2 update

---
+++ Day2/routing.js
@@ -0,0 +1,20 @@
+const server=require("http")
+server.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.write("this is my first page")
+    }
+    else
+        if(req.url==='/about'){
+            res.write("this is about page")
+        }
+        else
+            if(req.url==="/contact"){
+                res.write("this is contact page")
+            }
+            else{
+                res.write("404 page not found")
+            }
+res.end("")
+}).listen(5000,()=>{
+    console.log("server is started")
+})
---
+++ Day2/server.js
@@ -0,0 +1,7 @@
+// const server=require("http")
+// server.createServer((req,res)=>{
+//     res.write("<h1>this is my first server</h1>")
+//     res.end("")
+// }).listen(4000,()=>{
+//     console.log("server is started")
+// })
